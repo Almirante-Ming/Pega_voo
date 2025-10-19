02e7d27b36e4427ea8edf47fd3c1c2678b4bceb4
Gerson Bruno <gbrunosan159@gmail.com>
2025-10-19 17:43:31 -0400

    pagina de cadastro

---
+++ src/web/pages/cadastro/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="p-4">
+    <h2 class="text-2xl font-bold mb-4 text-grayScale-900">Essa é a pagina de cadastro</h2>
+   
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
