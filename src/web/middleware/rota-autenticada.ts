bc70abdabd8d05a3aeed7904f6cce48d1e8ed24e
gbrunosan <gbrunosan159@gmail.com>
2025-10-21 20:03:51 -0400

    tirando logs

--- src/web/middleware/rota-autenticada.ts
+++ src/web/middleware/rota-autenticada.ts
@@ -1,16 +1,11 @@
-import { useStoreDeUsuario } from "~/store/useStoreUsuario"; // Corrija o caminho para ~/stores
+import { useStoreDeUsuario } from "~/store/useStoreUsuario";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const storeDeUsuario = useStoreDeUsuario();  
 
-  // Carregar o token do localStorage antes de qualquer verificação
   storeDeUsuario.loadTokenFromStorage();
 
-  // Log para verificar se o token foi carregado corretamente
-  console.log("Token carregado:", storeDeUsuario.getToken());  
-
-  // Verificar se o token não existe ou se está expirado
   if (!storeDeUsuario.getToken() || storeDeUsuario.isTokenExpired()) {
-    return navigateTo('/login'); // Redirecionar para o login se o token for inválido ou expirado
+    return navigateTo('/login');
   }
 });
